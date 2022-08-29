e2beb6f4cb4b13679c13a52cdc4e04d35384c651
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-08-29 21:44:50 +0530

    Day 12

--- Day 12/main.py
+++ Day 12/main.py
@@ -5,5 +5,6 @@ Krishna['country of birth'] = 'India'
 Krishna['favorite language'] = 'Python'
 Krishna['favorite color'] = 'Green'
 Krishna['activity'] = 'coding'
+Krishna['event'] = '30DaysOfCode'
 
 print(Krishna)
