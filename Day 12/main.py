c3f4f5b43ea7a5e245e5cb4be432459ad615fab6
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-08-29 21:43:48 +0530

    Day 13

---
+++ Day 12/main.py
@@ -0,0 +1,9 @@
+Krishna = dict()
+Krishna['name'] = 'Krishna Agarwal'
+Krishna['age'] = 'probably 15'
+Krishna['country of birth'] = 'India'
+Krishna['favorite language'] = 'Python'
+Krishna['favorite color'] = 'Green'
+Krishna['activity'] = 'coding'
+
+print(Krishna)
