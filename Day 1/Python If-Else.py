6f309f12dfb1b25e633533abea0f1b15112abe4f
Mohd Quamar Tyagi <104281681+Tyagiquamar@users.noreply.github.com>
2022-10-01 03:05:24 +0530

    Update Python If-Else.py

--- Day 1/Python If-Else.py
+++ Day 1/Python If-Else.py
@@ -16,7 +16,7 @@ if n%2!=0:
 else:
     if n>=2 and n<=5:
         print("Not Weird")
-    if n>=6 and n<=20:
+    elif n>=6 and n<=20: #it is advisable to use proper if else ladder
         print("Weird")
-    if n>20:
+    elif n>20:
         print("Not Weird")
