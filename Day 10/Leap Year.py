5ea3e9eaa9fc2fc7eae1ea10343a2424009a3f5f
Mohd Quamar Tyagi <104281681+Tyagiquamar@users.noreply.github.com>
2022-10-01 13:21:42 +0530

    Update Leap Year.py
    Century year logic added

--- Day 10/Leap Year.py
+++ Day 10/Leap Year.py
@@ -1,7 +1,8 @@
 print("Enter the year to check whether it is leap year or not:")
 year = int(input())
-
-if year % 4 == 0:
+if (year % 400 == 0) and (year % 100 == 0): #added century year condition as previios logic was wrong for century year
+    print(year,"is a leap year")
+elif( year % 4 == 0) and (year%100!=0):
     print (year,"is a leap year.")
 else:
     print (year,"isn't a leap year.")
